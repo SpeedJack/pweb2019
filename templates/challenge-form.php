1c6e3a17de44f6da6a6b3660ae21ad32c8bd9f54
Niccolò Scatena <speedjack95@gmail.com>
2019-07-15 18:54:57 +0200

    Disable autocomplete on challenge-form

--- templates/challenge-form.php
+++ templates/challenge-form.php
@@ -5,7 +5,7 @@
  * @copyright GNU General Public License, version 3
  */
 ?>
-<form id="challenge-form" class="modal-form" autocomplete="on" data-actionurl="<?= $application->buildLink('__current', 'solve') ?>" onsubmit="return false;">
+<form id="challenge-form" class="modal-form" autocomplete="off" data-actionurl="<?= $application->buildLink('__current', 'solve') ?>" onsubmit="return false;">
 	<input type="hidden" id="challid" name="challid" value="<?= $chall->getId() ?>">
 	<input type="text" placeholder="<?= __('Enter Flag: flag{.....}') ?>" id="challflag" name="challflag" title="<?= __('Flag must have this format: flag{some text}.') ?>" pattern="<?= $application->config['form_validation']['flag_regex'] ?>" maxlength="<?= $application->config['form_validation']['flag_maxlength'] ?>" autofocus required>
 	<button type="submit" onclick="if (document.getElementById('challflag').checkValidity()) setModalRedirect('<?= $application->buildLink('__current') ?>');"><?= __('Submit') ?></button>
